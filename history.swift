2b816d8bc099e2336ea57a672c0361ee09ed1029
takumiiiil <takuniwo@yandex.com>
2020-09-01 16:00:02 +0900

    メソッド作ってまとめた

---
+++ history.swift
@@ -0,0 +1,9 @@
+//
+//  history.swift
+//  uobei
+//
+//  Created by takumi on 2020/09/01.
+//  Copyright © 2020 東竜生. All rights reserved.
+//
+
+import Foundation
---
+++ splitViewController2.swift
@@ -0,0 +1,9 @@
+//
+//  splitViewController2.swift
+//  uobei
+//
+//  Created by takumi on 2020/08/31.
+//  Copyright © 2020 東竜生. All rights reserved.
+//
+
+import Foundation
