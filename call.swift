db4f6cf6b31c5d5e0d606280337af18fe2184120
takumiiiil <takuniwo@yandex.com>
2020-09-09 14:57:03 +0900

    call

--- call.swift
+++ call.swift
@@ -15,11 +15,32 @@ class call: UIViewController {
     // ImageViewを.定義する.
     var myImageView: UIImageView!
     var addTimer = Timer()
-
+    
+    //viewの設定
+            func viewSetting(SViewController:UIViewController){
+               //アニメーションを設定する.
+               SViewController.modalTransitionStyle = .flipHorizontal
+               //Viewの移動する.
+               SViewController.modalPresentationStyle = .fullScreen
+               self.present(SViewController, animated: true, completion: nil)
+            }
+    //画像の設定
+       func imageSetteng(picture:String){
+           // 画像を設定する.
+           let myInputImage = CIImage(image: UIImage(named: picture)!)
+           // ImageViewを.定義する.
+           var myImageView: UIImageView!
+           // UIImageViewを作成する.
+           myImageView = UIImageView(frame: UIScreen.main.bounds)
+           myImageView.image = UIImage(ciImage: myInputImage!)
+           self.view.addSubview(myImageView)
+       }
     override func viewDidLoad() {
         loadView()//videoplayerを破棄
-        print("overfuncのloadviewはした")
         super.viewDidLoad()
+        
+        let button = makeButton()//m:backgrand,e:picture,e:border
+        audioPlayerInstance.prepareToPlay()
         myImageView = UIImageView(frame: self.view.frame)
         myImageView.image = UIImage(ciImage: myInputImage!)
         self.view.addSubview(myImageView)
@@ -27,31 +48,18 @@ class call: UIViewController {
             let bc = makeBarcord()
             self.view.addSubview(bc.make(string: "12-34")!)
         }else{
-            let qrc = makeQrcode()
-            self.view.addSubview(qrc.make(sum: appDelegate.qr_string))
+//            let qrc = makeQrcode()
+//            self.view.addSubview(qrc.make(sum: appDelegate.qr_string))
         }
         
-        //ボタン作成メソッド
-        func makeButton(xv:Int,yv:Int,wv:Int,hv:Int,f:Int,b:String,c:Int){
-            let button: UIButton = UIButton(frame: CGRect(x:CGFloat(xv), y:CGFloat(yv), width: CGFloat(wv), height: CGFloat(hv)))
-            button.backgroundColor = UIColor.white
-            button.layer.borderColor = UIColor.brown.cgColor
-            button.layer.borderWidth = 1.5
-            button.setTitle("\(b)", for: .normal)
-            button.layer.cornerRadius = 3.0
-            button.setTitleColor(UIColor.red, for: .normal)
-            button.titleLabel?.numberOfLines = 2//0:無限改行,2:2行表示
-            button.titleLabel?.font =  UIFont.systemFont(ofSize: 50)
-            button.tag = c
-            button.addTarget(self, action: #selector(B1(sender:)), for: .touchUpInside)
-            // ボタンを追加する.
-            self.view.addSubview(button)
-        }
+   
+        
+     
         
         //ボタン作成
-        makeButton(xv:80,yv:500,wv:280,hv:180,f:20,b:"会計",c:0)
-        makeButton(xv:380,yv:500,wv:280,hv:180,f:20,b:"キャンセル",c:1)
-        makeButton(xv:680,yv:500,wv:280,hv:180,f:20,b:"戻る",c:2)
+        self.view.addSubview(button.make(xv:80,yv:500,wv:280,hv:180,f:20,b:"会計",c:0,t:1))
+        self.view.addSubview(button.make(xv:380,yv:500,wv:280,hv:180,f:20,b:"キャンセル",c:1,t:1))
+        self.view.addSubview(button.make(xv:680,yv:500,wv:280,hv:180,f:20,b:"戻る",c:2,t:1))
     }
     
 
@@ -108,65 +116,37 @@ class call: UIViewController {
         return capturedImage
     }
     
-    @objc internal func B1(sender: UIButton){
+    @objc internal func selection(sender: UIButton){
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
-//        let qrc = makeQrcode()
         let view = makeView()
         switch sender.tag{
-            case 0://言語切替
+            case 0:
                 let image:UIImage = getScreenShot()
                 appDelegate.sc_image = image
                 showPrinterPicker()
-                let date = Date()
-                let dateAndTime = date.formattedDateWith(style: .time)
-                ab.outTime = dateAndTime
-
-                // (1)Realmのインスタンスを生成する
-                let realm = try! Realm()
-                // (2)書き込みトランザクション内でデータを追加する
-                try! realm.write {
-                    realm.add(ab)
+                imageSetteng(picture: "wait.jpg")
+                
+                //遅延処理
+                DispatchQueue.main.asyncAfter(deadline: .now() + 8) {
+                    let SViewController: UIViewController = camera()
+                              //アニメーションを設定する.
+                              SViewController.modalTransitionStyle = .flipHorizontal
+                              //Viewの移動する.
+                              SViewController.modalPresentationStyle = .fullScreen
+                     self.present(SViewController, animated: true, completion: nil)
                 }
-                let SViewController: UIViewController = ViewController()
-                //アニメーションを設定する.
-                SViewController.modalTransitionStyle = .flipHorizontal
-                //Viewの移動する.
-                SViewController.modalPresentationStyle = .fullScreen
-                self.present(SViewController, animated: true, completion: nil)
+            case 1:
+                    let SViewController: UIViewController = kaikei2()
+                    self.viewSetting(SViewController: SViewController)
+                 
             case 2:
-                let SViewController: UIViewController = ViewController()
-                //アニメーションを設定する.
-                SViewController.modalTransitionStyle = .flipHorizontal
-                //Viewの移動する.
-                SViewController.modalPresentationStyle = .fullScreen
-                self.present(SViewController, animated: true, completion: nil)
-                //audioPlayerInstance.play()
-            case 3://テイクアウト
-                print("ok3")
-            case 4://注文履歴
-                print("ok4")
-            case 5://店員呼び出し
-                print("ok5")
-            case 6:
-                print("ok6")
-            case 7:
-                print("ok7")
-            case 8:
-                print("ok8")
-            case 9:
-                print("ok9")
-            case 10:
-                print("ok10")
-            case 11:
-                print("ok11")
+                    let SViewController: UIViewController = ViewController()
+                    self.viewSetting(SViewController: SViewController)
+       
             case 12:
                 addTimer.invalidate()
-                let SViewController: UIViewController = call()
-                //アニメーションを設定する.
-                SViewController.modalTransitionStyle = .flipHorizontal
-                //Viewの移動する.
-                SViewController.modalPresentationStyle = .fullScreen
-                self.present(SViewController, animated: true, completion: nil)
+               let SViewController: UIViewController = ViewController()
+                self.viewSetting(SViewController: SViewController)
                 self.view.addSubview(view.make())
             case 22://動画再生中のタップ
                 addTimer.invalidate()
